796d24599b5fd69fee9535c77ee7240d5b2245db
School <school@macbook-pro.lan>
2025-02-09 22:09:53 -0700

    added starting area

--- game.py
+++ game.py
@@ -5,3 +5,11 @@ player_name = input("What is your name adventurer?")
 
 # Concatenate string into single print
 print(f"Welcome, {player_name}! Your journey begins now.")
+
+# Describe this starting area
+starting_area = """
+You find yourself in a moonlit forest.
+The sound of wind and leaves fills the area.
+A faint path leads deeper into the unknown....
+"""
+print(starting_area)
